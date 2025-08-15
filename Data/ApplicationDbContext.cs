11741fecee11f0dccb4b039e0c9864a3bb8a013b
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-15 21:13:12 +0300

    Создал контекст БД создав файл ApplicationDbContext.cs подключил пакет Microsoft.EntityFrameworkCore Изменил название сущности Massage на MassageV, т.к. вероятнг проблема в том, что сущность имеет такое же название как и имя проекта.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Massage.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace Massage.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<MassageV> Massages { get; set; }
+    public DbSet<TimeSlot> TimeSlots { get; set; }
+    public DbSet<Booking> Bookings { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // Добавляем отношения между таблицами
+        modelBuilder.Entity<Booking>()
+            .HasOne(b => b.TimeSlot)
+            .WithMany()
+            .OnDelete(DeleteBehavior.NoAction); // Если удаляется запись, слот остаётся неизменным
+
+        modelBuilder.Entity<Booking>()
+            .HasOne(b => b.SelectedMassage)
+            .WithMany()
+            .OnDelete(DeleteBehavior.NoAction); // Аналогично, если удалить запись, вид массажа остается
+    }
+}
--- Models/Massage.cs
+++ Models/Massage.cs
@@ -2,7 +2,7 @@
 
 namespace Massage.Models;
 
-public class Massage
+public class MassageV    
 {
     public int Id { get; set; }
 
