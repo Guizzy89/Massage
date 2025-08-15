31b8ea82b33cd7019083fc0fee2f5cb386c89bc0
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-15 22:00:42 +0300

    Gереходим к разработке фронтенда. Создадим простой интерфейс для отображения списка доступных услуг массажа, выбора времени визита и отправки заявки на запись. Приложение использует архитектуру ASP.NET Core MVC.
    // Структура папок для ASP.NET Core MVC
    1. Controllers: Контроллеры приложения.
    2. Models: Модели данных.
    3. Views: Файлы представлений (.cshtml).
    4. wwwroot: Статические ресурсы (CSS, JavaScript, изображения и др.).
    
    Почему не в ?
    
    Папка Pages используется в архитектуре Razor Pages, которая отличается от MVC. В Razor Pages страницы содержат как логику, так и разметку, что упрощает создание небольших приложений. В вашем случае, поскольку у вас есть контроллеры, это указывает на использование MVC, и файлы представлений должны быть размещены в папке Views.

---
+++ Views/Massage/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Massage.Views.Massage
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
