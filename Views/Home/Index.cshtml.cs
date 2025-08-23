65fc8b3bd92fd107ce45589ac71638cdc2c81590
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-23 15:17:26 +0300

    добавил через PostMan примеры массажей

---
+++ Views/Home/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Massage.Views.Home
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
