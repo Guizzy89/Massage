3a75bf5588add1aced03f4d063457d3380fef5d0
Guizzy89 <guizzyilgizgareev@gmail.com>
2025-08-23 15:23:24 +0300

    Убедитесь, что маршруты в контроллерах и в настройках приложения соответствуют ссылкам. В файле Program.cs должна быть настроена маршрутизация:

--- Program.cs
+++ Program.cs
@@ -25,6 +25,11 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
 app.MapControllers();
 app.UseRouting();
 
